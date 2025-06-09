775683529d12aacaebac0e88cbc62a76937e4983
Eldho Paul <161152825+eldhopaul86@users.noreply.github.com>
2025-06-09 23:38:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -148,4 +148,5 @@ def handler(request):
     return app(request.environ, lambda *args: None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
